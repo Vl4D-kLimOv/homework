5841fc4e9df1394b101f30cca4e05d38a1a0b39f
Vl4D-kLimOv <vkebex010@gmail.com>
2024-11-14 11:51:55 +0300

    do homework with SOLID

--- main.js
+++ main.js
@@ -1,27 +1,81 @@
-// Ситуация 1
-const numbers = [-10, 5, 0, 3, -1, 8, -7];
-const positiveNumbers = numbers.filter(number => number > 0);
-console.log(positiveNumbers);
-// Ситуация 2
-const names = ['alice', 'bob', 'charlie', 'david'];
-const upperCaseNames = names.map(name => name.toUpperCase());
-console.log(upperCaseNames);
-// Ситуация 3
-const numbersToSort = [5, 3, 8, 1, 4];
-numbersToSort.sort((a, b) => a-b);
-console.log(numbersToSort);
-// Ситуация 4
-const numbersToSum = [10, 20, 30, 40, 50];
-let sum = numbersToSum.reduce((sum, number)=>sum+number, 0)
-console.log(sum)
-// Ситуация 5
-tasks = ["Купить продукты", "Сделать домашнее задание", "Позвонить другу"];
-console.log(tasks.length);
-tasks.push("Убраться в комнате");
-tasks.shift();
-console.log(tasks);
-// Ситуация 6
-const forbiddenWords = ['spam', 'advertisement', 'scam'];
-const message = "This is a spam message with scam";
-const containsForbiddenWord = forbiddenWords.some(word => message.includes(word));
-console.log(containsForbiddenWord);
+// 1. Нарушен принцип инверсии зависимостей
+//Исправленный код
+class Service { //Создаем общий интерфейс
+    sendMessage(message) {
+        throw new Error("это интерфейс");
+    }
+}
+
+class EmailService extends Service{ //Переопределяем этот интерфейс под нужную задачу
+    sendMessage(message) {
+        console.log(`Отправка email с сообщением: ${message}`);
+    }
+}
+
+class Notification { 
+  constructor(service) {
+      this.service = service;
+  }
+ 
+  notify(message) {
+      this.service.sendMessage(message);
+  }
+}
+ 
+const notification = new Notification(new EmailService); 
+notification.notify("Важное сообщение");
+ 
+ 
+ //2. Нарушен принцип подстановки Барбары Лисков
+ //Исправленный код: 
+
+class Bird {
+    fly() {
+        console.log("Птица летит");
+    }
+    action() {
+        this.fly(); //создаем новый общий метод, который будем вызввать при действии. Передаем в него то, что птица делать умеет делать
+    }
+}
+
+class Duck extends Bird {}
+class Penguin extends Bird {
+
+    action() {
+        this.swim(); //вместо fly используем swim
+    }
+    swim() {
+        console.log("Птица плавает")
+    }
+    fly() {
+        throw new Error("Пингвины не умеют летать");
+    }
+
+}
+
+const birds = [new Duck(), new Penguin()];
+birds.forEach(bird => bird.action()); //вызывем общщий метод
+ 
+
+ //3. Был нарушен принцип единственной ответственности
+ //Исправленный код:
+
+class Database { //Создаем новый класс для для базы данных, у которого будем вызывать соответствующий метод
+  save(user) {
+      console.log(`Сохранение пользователя ${user.getName()} в базу данных`);
+  }
+}
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+ 
+    getName() {
+        return this.name;
+    }
+}
+
+const user = new User("Алексей", 30);
+const database = new Database;
+database.save(user);
