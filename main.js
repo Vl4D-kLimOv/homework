a663183d314ed1ca32954afe1a76625b0fb4ac96
Vl4D-kLimOv <vkebex010@gmail.com>
2024-11-26 10:09:55 +0300

    homework with functions

--- main.js
+++ main.js
@@ -1,49 +1,76 @@
-// 1.
-function sum() {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i]; 
+// 1 Задание
+const reverseWords = function(str) {
+    return str.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+};
+
+const reversedWords = reverseWords('Hello World');
+console.log(reversedWords); // Вывод: "olleH dlroW"
+
+// 2 Задание
+function* arrayIterator(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        yield arr[i]; 
     }
-    return sum; 
 }
 
-// 2.
-function filterEvens() {
-    return Array.from(arguments).filter(num => num % 2 === 0);
-}
-console.log(filterEvens(1, 2, 3, 4, 5, 6));
+const gen = arrayIterator(['apple', 'banana', 'cherry']);
+console.log(gen.next().value); // 'apple'
+console.log(gen.next().value); // 'banana'
+console.log(gen.next().value); // 'cherry'
+console.log(gen.next().done);  // true
 
-// 3.
-const obj = { 
-    name: 'Object', 
-    getName: function () { 
-      return this.name; 
-    } 
-}; 
-
-const getName = obj.getName; 
-console.log(getName()); //undefined
-const newGetName = obj.getName.bind(obj)
-console.log(newGetName());
-
-// 4.
-function greet(){
-console.log(`Hi, i'm ${this.name}`)
+// 3 Задание
+function map(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i], i, array));
+    }
+    return newArray;
 }
 
-const person = { name: 'Alice' };
+const initArr = [1, 2, 3, 4, 5];
+const callback = (item, index, array) => item * 2;
+const res = map(initArr, callback);
+console.log(initArr); // [1, 2, 3, 4, 5]
+console.log(res);     // [2, 4, 6, 8, 10]
 
-greet.call(person)
-greet.apply(person)
-const contextGreet = greet.bind(person)
-contextGreet()
+// 4 Задание
+function createLogger(prefix) {
+    return function(message) {
+        console.log(`${prefix}${message}`);
+    };
+}
 
-// Уровень 1
-const sumTwo = (a, b) => a + b;
+const errorLog = createLogger('ERROR: ');
+errorLog('Пофикси меня!'); // ERROR: Пофикси меня!
 
-// Уровень 2
-const doubleArray = (arr) => arr.map(num => num * 2);
+const debugLog = createLogger('DEBUG: ');
+debugLog('Важная отладочная информация!'); // DEBUG: Важная отладочная информация!
 
-// Уроыень 3
-const getStringLengths = (arr) => arr.map(str => str.length);
-  
+const infoLog = createLogger('INFO: ');
+infoLog('Тестовое информационное сообщение.'); // INFO: Тестовое информационное сообщение.
+
+// 5 Задание
+const sumArray = function sum(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+    return arr[0] + sum(arr.slice(1));
+};
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(sumArray(numbers));
+
+// 6 Задание
+(function() {
+    const randomNumbers = [...Array(5)].map(() => Math.floor(Math.random() * 100));
+
+    const min = Math.min(...randomNumbers);
+    const max = Math.max(...randomNumbers);
+
+    console.log(randomNumbers);
+    console.log(min);
+    console.log(max);
+})();
+
+
