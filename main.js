6c97dcc0c269cc0b0724b07123d95fe6bdaa4f2c
Vl4D-kLimOv <vkebex010@gmail.com>
2024-12-10 00:28:10 +0300

    do homework with asinc

--- main.js
+++ main.js
@@ -1,187 +1,138 @@
-// Уровень 1
-// Задание 1
-class Book {
-    constructor(title, author) {
-        this.title = title;   
-        this.author = author;
-    }
+//1 Уровень
+//1 Задание
 
-    getBookInfo() {
-        return `Название: "${this.title}", Автор: ${this.author}`;
-    }
+function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
 }
 
-const myBook = new Book('1984', 'Джордж Оруэлл');
+wait(10000) 
+    .then(() => {
+        console.log('Промис разрешен!'); 
+    });
 
-console.log(myBook.getBookInfo()); 
+//2 Задание
 
-// Задание 2
-class Person {
-    #name;
-    #age; 
-
-    constructor(name, age) {
-        this.#name = name; 
-        this.#age = age; 
-    }
-
-    getName() {
-        return this.#name; 
-    }
-
-    getAge() {
-        return this.#age;
-    }
+function step1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Шаг 1 выполнен");
+        }, 1000);
+    });
 }
 
-const person = new Person('Влад', 19);
-
-console.log(`Имя: ${person.getName()}`); 
-console.log(`Возраст: ${person.getAge()}`);
-
-// Задание 3
-class Animal {
-
-    constructor(name, age) {
-        this.name = name; 
-        this.age = age; 
-    }
-
-    describe() {
-        return `Имя ${this.name}, возраст: ${this.age} лет.`;
-    }
-
-    sound() {
-        return `${this.name} издает звук.`;
-    }
+function step2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Шаг 2 выполнен");
+        }, 2000);
+    });
 }
 
-class Dog extends Animal {
-    sound() {
-        return `${this.name} лает!`;
-    }
-}
+step1()
+    .then((result1) => {
+        console.log(result1); 
+        return step2(); 
+    })
+    .then((result2) => {
+        console.log(result2);
+    })
 
-const animal = new Animal('Медведь', 5);
-console.log(animal.describe()); 
-console.log(animal.sound());    
-
-const dog = new Dog('Рекс', 3);
-console.log(dog.describe()); 
-console.log(dog.sound()); 
-
-// Уроовень 2
-// Задание 1
-class newPerson {
-    _name;
-
-    constructor(name) {
-        this._name = name;
-    }
-
-    getName() {
-        return this._name;
-    }
-
-    setName(newName) {
-        this._name = newName;
-    }
-}
-
-class Student extends newPerson {
-    _grade;
-
-    constructor(name, grade) {
-        super(name); 
-        this.setGrade(grade);
-    }
-
-    getGrade() {
-        return this._grade;
-    }
-
-    setGrade(newGrade) {
-        if (newGrade < 1 || newGrade > 5) {
-            console.log('Ошибка: Оценка должна быть в пределах от 1 до 5.');
-            return; 
-        }
-        this._grade = newGrade;
-    }
-}
-
-const newperson = new newPerson('Иван Иваныч');
-console.log(newperson.getName()); 
-
-newperson.setName('Саша');
-console.log(newperson.getName()); 
-
-const student = new Student('Вася', 4);
-console.log(student.getName()); 
-console.log(student.getGrade()); 
-
-student.setGrade(5);
-console.log(student.getGrade());
-
-student.setGrade(6);
-
-// Задание 2
-class Shape {
-
-    static count = 0;
-
-    #name;
-
-    constructor(name) {
-        this.#name = name;
-        Shape.count++;
-    }
-
-    getArea() {
-        print("Метод 'getArea()' должен быть переопределен в подклассе.");
-    }
-
-    getName() {
-        return this.#name;
-    }
-
-    static getCount() {
-        return Shape.count;
-    }
+//3 Задание
+function task1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Задача 1 завершена");
+        }, 1000);
+    });
 }
 
 
-class Rectangle extends Shape {
-
-    constructor(width, height) {
-        super('Rectangle'); 
-        this.width = width;
-        this.height = height;
-    }
-
-
-    getArea() {
-        return this.width * this.height;
-    }
+function task2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Задача 2 завершена");
+        }, 2000); 
+    });
 }
 
-
-class Circle extends Shape {
-
-    constructor(radius) {
-        super('Circle'); 
-        this.radius = radius;
-    }
-
-    getArea() {
-        return Math.PI * this.radius ** 2;
-    }
+function task3() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Задача 3 завершена");
+        }, 3000); 
+    });
 }
 
+Promise.all([task1(), task2(), task3()])
+    .then((results) => {
+        results.forEach(result => console.log(result));
+    })
 
-const rectangle = new Rectangle(5, 10);
-const circle = new Circle(4);
+//2 Уровень
+//1 Задание
 
-console.log(`${rectangle.getName()} - Площадь: ${rectangle.getArea()}`); 
-console.log(`${circle.getName()} - Площадь: ${circle.getArea()}`); 
+const usersUrl = 'https://jsonplaceholder.typicode.com/users'; 
+const postsUrl = 'https://jsonplaceholder.typicode.com/posts'; 
+const commentsUrl = 'https://jsonplaceholder.typicode.com/comments';
 
-console.log(`Количество созданных фигур: ${Shape.getCount()}`); 
+function fetchData(url) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                return Promise.reject();
+            }
+            return response.json();
+        });
+}
+
+Promise.all([
+    fetchData(usersUrl),
+    fetchData(postsUrl),
+    fetchData(commentsUrl)
+])
+.then(([users, posts, comments]) => {
+    console.log('Пользователи:', users);
+    console.log('Посты:', posts);
+    console.log('Комментарии:', comments);
+})
+.catch(() => {
+    console.log('Ошибка при получении данных');
+});
+
+//2 Задание
+const usersUrlNew = 'https://jsonplaceholder.typicode.com/users';
+const postsUrlNew = 'https://jsonplaceholder.typicode.com/posts';
+const commentsUrlNew = 'https://jsonplaceholder.typicode.com/comments';
+
+const fetchWithTimeout = (url) => {
+    return Promise.race([
+        fetch(url).then(response => {
+            if (!response.ok) {
+                return Promise.reject('Ошибка при получении данных');
+            }
+            return response.json();
+        }),
+        new Promise((_, reject) =>
+            setTimeout(() => reject('Запрос превышает время ожидания'), 5000)
+        )
+    ]);
+};
+
+Promise.all([
+    fetchWithTimeout(usersUrlNew),
+    fetchWithTimeout(postsUrlNew),
+    fetchWithTimeout(commentsUrlNew)
+])
+.then(results => {
+    const [users, posts, comments] = results;
+    console.log('Пользователи:', users);
+    console.log('Посты:', posts);
+    console.log('Комментарии:', comments);
+})
+.catch(error => {
+    console.log(error);
+});
