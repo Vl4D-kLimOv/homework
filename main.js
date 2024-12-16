99785b07f2e910884b36b404c23117526df1b99a
Vl4D-kLimOv <vkebex010@gmail.com>
2024-12-17 01:50:23 +0300

    do homework with DOM

--- main.js
+++ main.js
@@ -37,28 +37,31 @@ divideNumbers(10, 'x');
 divideNumbers('x', 'y');
 
 
+
+async function fetchUserJson(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Ошибка получения данных');
+    }
+    return await response.json();
+}
+
+function processUserData(users) {
+    users.forEach(user => {
+        console.log(user.name);
+    });
+}
+
+
 async function fetchUserData() {
     const url = 'https://jsonplaceholder.typicode.com/users';
-
     try {
-        const response = await fetch(url);
-
-
-        if (!response.ok) {
-            throw new Error('Ошибка получения данных');
-        }
-
-        const users = await response.json();
-
-        users.forEach(user => {
-            console.log(user.name);
-        });
-
+        const users = await fetchUserJson(url);
+        processUserData(users);
     } catch (error) {
-
         console.error(error.message);
     }
-};
+}
 
 fetchUserData();
 
@@ -70,49 +73,60 @@ async function fetchJson(url) {
     return response.json();
 }
 
+async function getEvenIdPosts() {
+    const posts = await fetchJson('https://jsonplaceholder.typicode.com/posts');
+    return posts.filter(post => post.id % 2 === 0);
+}
+
+async function getCommentsForPosts(posts) {
+    const commentsPromises = posts.map(post =>
+        fetchJson(`https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
+    );
+    return await Promise.all(commentsPromises);
+}
+
+function findLongestComments(evenIdPosts, commentsArray) {
+    return evenIdPosts.map((post, index) => {
+        const comments = commentsArray[index];
+        let longestComment = { body: '' };
+
+        comments.forEach(comment => {
+            if (comment.body.length > longestComment.body.length) {
+                longestComment = comment;
+            }
+        });
+
+        return { postId: post.id, longestComment: longestComment.body };
+    });
+}
+
+async function saveResults(results) {
+    const postPromises = results.map(result => 
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(result)
+        })
+    );
+
+    await Promise.all(postPromises);
+}
+
 async function fetchUserDataNew() {
     try {
-
-        const posts = await fetchJson('https://jsonplaceholder.typicode.com/posts');
+        const evenIdPosts = await getEvenIdPosts();
+        const commentsArray = await getCommentsForPosts(evenIdPosts);
+        const results = findLongestComments(evenIdPosts, commentsArray);
+        await saveResults(results);
         
-        const evenIdPosts = posts.filter(post => post.id % 2 === 0);
-        
-        const commentsPromises = evenIdPosts.map(post => 
-            fetchJson(`https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
-        );
-        
-        const commentsArray = await Promise.all(commentsPromises);
-        
-        const results = evenIdPosts.map((post, index) => {
-            const comments = commentsArray[index];
-            let longestComment = { body: '' };
-            
-            comments.forEach(comment => {
-                if (comment.body.length > longestComment.body.length) {
-                    longestComment = comment;
-                }
-            });
-            
-            return { postId: post.id, longestComment: longestComment.body };
-        });
-        
-        const postPromises = results.map(result => 
-            fetch('https://jsonplaceholder.typicode.com/posts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(result)
-            })
-        );
-
-        await Promise.all(postPromises);
         console.log('Результаты успешно сохранены');
-        
     } catch (error) {
         console.error(error.message);
     }
 }
+
 
 
 fetchUserDataNew();
